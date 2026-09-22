b488c60d741a96d47d425cd25370ea15e5d2a21d
Ankit Bhumeshwar Katre <ankitkatre690@gmail.com>
2026-09-22 14:22:06 +0530

    feat(dispatch): add exceptions.py

---
+++ apps/backend/app/modules/dispatch/exceptions.py
@@ -0,0 +1,18 @@
+class DispatchError(Exception):
+    pass
+
+
+class EmergencyNotFound(Exception):
+    pass
+
+
+class NoAvailableResponder(Exception):
+    pass
+
+
+class NoAvailableHospitalResource(Exception):
+    pass
+
+
+class DispatchAlreadyExists(Exception):
+    pass
