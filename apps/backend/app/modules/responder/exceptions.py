a451ec4803560861b8424494a96b9de647cf2f93
Ankit Bhumeshwar Katre <ankitkatre690@gmail.com>
2026-09-22 13:12:09 +0530

    feat(responder): implement responder module

---
+++ apps/backend/app/modules/responder/exceptions.py
@@ -0,0 +1,22 @@
+class ResponderProfileNotFound(Exception):
+    pass
+
+
+class ResponderProfileAlreadyExists(Exception):
+    pass
+
+
+class InvalidResponderRole(Exception):
+    pass
+
+
+class EmergencyAssignmentNotFound(Exception):
+    pass
+
+
+class EmergencyNotFound(Exception):
+    pass
+
+
+class AssignmentAlreadyExists(Exception):
+    pass
