eb72b06529c9f89dd1d3c700298b4b631cd3aaa3
Ankit Bhumeshwar Katre <ankitkatre690@gmail.com>
2026-09-24 13:31:59 +0530

    feat(frontend): register FCM background handler

--- apps/frontend/lib/main.dart
+++ apps/frontend/lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'core/notifications/push_notification_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (!const bool.fromEnvironment('dart.library.html')) {
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  }
+
   runApp(const ProviderScope(child: LifeLinkApp()));
 }
